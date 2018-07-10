88d7696f70f02250b7edfc859972e83b734f6cad
Sidneyyy <catitude14@outlook.com>
2018-07-10 13:39:37 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -5,7 +5,7 @@ puts "Let's plan your trip! Where would you like to stay?"
 
 stay = gets.chomp.capitalize  
 
-puts "I would like to stay in #{stay.capitalize}."
+puts "I would like to stay in #{stay}."
 
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
